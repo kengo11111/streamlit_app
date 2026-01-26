4bc0e484f53a64a02baa5a3dd3d9faaf0d00cc62
kengo11111 <t125016@ed.sus.ac.jp>
2026-01-26 11:40:58 +0900

    縦持ちデータに変換

--- 最終課題/app.py
+++ 最終課題/app.py
@@ -12,3 +12,16 @@ activity = st.sidebar.selectbox(
 
 df = pd.read_csv(f'{activity}.csv', encoding='shift_jis')
 
+# 年度と地域は識別用の列として保持する
+id_cols = ['調査年', '地域']
+
+# Mから始まる列（性別・年齢層ごとの平均時間）だけを抽出する
+value_cols = [c for c in df.columns if c.startswith('M')]
+
+# melt を利用し、講義で扱った資料のように横に並んだ年齢層・性別ごとの平均時間の列を横に並んだ指標列を1列にまとめて縦持ちデータに変換する
+df_long = df.melt(
+    id_vars=id_cols,
+    value_vars=value_cols,
+    var_name='raw_col',
+    value_name='minutes'
+)
