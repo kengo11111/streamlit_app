31086c3798775c64bd37ef5fb8d0c97952d7f8c6
kengo11111 <t125016@ed.sus.ac.jp>
2026-01-26 11:30:21 +0900

    サイドバーの分析とファイルの読み込み

--- 最終課題/app.py
+++ 最終課題/app.py
@@ -5,3 +5,10 @@ import re
 
 st.title('時間使用調査（活動別・性別・年齢層）')
 
+activity = st.sidebar.selectbox(
+    '活動種別を選択してください',
+    ['Primary', 'Secondary', 'Tertiary']
+)
+
+df = pd.read_csv(f'{activity}.csv', encoding='shift_jis')
+
